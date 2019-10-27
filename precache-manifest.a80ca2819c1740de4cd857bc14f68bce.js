a5b95812d9cdbca7c8e21890bdaeb6e6e80e329a
alexds98 <47061045+alexds98@users.noreply.github.com>
2019-10-27 15:24:09 +0100

    Updates

--- precache-manifest.a80ca2819c1740de4cd857bc14f68bce.js
+++ precache-manifest.a80ca2819c1740de4cd857bc14f68bce.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92256337ed8f95292cfa73d3f9a3d6d5",
+    "revision": "b32d5dfd4674520810b6da1cea7173e0",
     "url": "/e_commerce_react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/e_commerce_react/static/css/2.47fb9b64.chunk.css"
   },
   {
-    "revision": "5f0228bebe8000c2dfd2",
+    "revision": "635cefacb813b0b22385",
     "url": "/e_commerce_react/static/css/main.20162d98.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/e_commerce_react/static/js/2.f39ca900.chunk.js"
   },
   {
-    "revision": "5f0228bebe8000c2dfd2",
-    "url": "/e_commerce_react/static/js/main.1a1cfe39.chunk.js"
+    "revision": "635cefacb813b0b22385",
+    "url": "/e_commerce_react/static/js/main.4b2f5e7d.chunk.js"
   },
   {
     "revision": "fd11947d8715c6cb4f6d",
